8a6fc4d49ca61dc6383e16dffef1d2d46b23f393
fyzq <974577177@qq.com>
2021-11-02 12:07:35 +0800

    Initial Commit

---
+++ phrase_scanner/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  phrase_scanner
+//
+//  Created by 周南 on 11/2/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
